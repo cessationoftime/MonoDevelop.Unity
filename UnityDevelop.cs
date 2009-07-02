040e01fb970108019d7e382e2121a45ebb40e9ed
Matthew Davey <matthew.davey@dotbunny.com>
2009-07-02 13:31:21 -0400

    Project reoginization and optimization

--- UnityDevelop.cs
+++ UnityDevelop.cs
@@ -1,102 +1,64 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.CodeDom;
-using System.Threading;
 
-using MonoDevelop.Core;
-using MonoDevelop.Ide.Gui;
-using MonoDevelop.Components.Commands;
-using MonoDevelop.Projects;
-using MonoDevelop.Projects.Text;
-using MonoDevelop.Projects.Dom;
-using MonoDevelop.Projects.Dom.Parser;
-using MonoDevelop.Projects.Dom.Output;
-using MonoDevelop.Ide.Gui.Content;
-using MonoDevelop.Projects.CodeGeneration;
-using MonoDevelop.Ide.Gui.Dialogs;
-using MonoDevelop.Ide.FindInFiles;
 
 namespace UnityDevelop
 {
-    public enum Commands
-    {
-        ViewManual,
-        ViewReference,
-        ViewScriptReference,
-        SearchManual,
-        SearchReference,
-        SearchScriptReference,
-    }
 
-    public class ViewManualHandler : CommandHandler
+    public class UnityHandler
     {
-        protected override void Run (object data)
-        {
-            System.Diagnostics.Process.Start("http://unity3d.com/support/documentation/Manual/index.html");
-        }
-    }
-    public class ViewReferenceHandler : CommandHandler
-    {
-        protected override void Run (object data)
-        {
-            System.Diagnostics.Process.Start("http://unity3d.com/support/documentation/Components/index.html");
-        }
-    }
-    public class ViewScriptReferenceHandler : CommandHandler
-    {
-        protected override void Run (object data)
-        {
-            System.Diagnostics.Process.Start("http://unity3d.com/support/documentation/ScriptReference/index.html");
-        }
-    }
+        private bool openInBrowser = true;
+        /*System.Web.HttpUtility.UrlEncode(text));*/
 
-    public class SearchManualHandler : CommandHandler
-    {
-        protected override void Run (object data)
-        {
-            // Get active documents text buffer
-            IEditableTextBuffer editor = IdeApp.Workbench.ActiveDocument.GetContent <IEditableTextBuffer>();
+        #region Overloaded Open
 
-            // Check for selection
-            if ( editor.SelectedText != null && editor.SelectedText.Length > 0 )
+        public  bool Open(Unity.Documentation target)
+        {
+            if ( this.openInBrowser )
             {
-                 // Create a search url string with the selected text as query:
-                 System.Diagnostics.Process.Start("http://www.google.com/search?hl=en&q=site%3Ahttp%3A%2F%2Funity3d.com%2Fsupport%2Fdocumentation%2FManual%2F+"
-                    + System.Web.HttpUtility.UrlEncode(editor.SelectedText));
+                switch (target)
+                {
+                    case Unity.Documentation.Manual:
+                        return this.Open(Unity.REMOTE_VIEW_MANUAL_URI);
+                    case Unity.Documentation.Reference:
+                        return this.Open(Unity.REMOTE_VIEW_REFERENCE_URI);
+                    case Unity.Documentation.ScriptReference:
+                        return this.Open(Unity.REMOTE_VIEW_SCRIPT_REFERENCE_URI);
+                }
             }
+            return false;
         }
-    }
-    public class SearchReferenceHandler : CommandHandler
-    {
-        protected override void Run (object data)
-        {
-            // Get active documents text buffer
-            IEditableTextBuffer editor = IdeApp.Workbench.ActiveDocument.GetContent <IEditableTextBuffer>();
 
-            // Check for selection
-            if ( editor.SelectedText != null && editor.SelectedText.Length > 0 )
-            {
-                 // Create a search url string with the selected text as query:
-                 System.Diagnostics.Process.Start("http://www.google.com/search?hl=en&q=site%3Ahttp%3A%2F%2Funity3d.com%2Fsupport%2Fdocumentation%2FComponents%2F+"
-                    + System.Web.HttpUtility.UrlEncode(editor.SelectedText));
-            }
-        }
-    }
-    public class SearchScriptReferenceHandler : CommandHandler
-    {
-        protected override void Run (object data)
+        public  bool Open(Unity.Documentation target, string query)
         {
-            // Get active documents text buffer
-            IEditableTextBuffer editor = IdeApp.Workbench.ActiveDocument.GetContent <IEditableTextBuffer>();
-
-            // Check for selection
-            if ( editor.SelectedText != null && editor.SelectedText.Length > 0 )
+            // Searching
+            if (query != null && query.Length > 0)
             {
-                 // Create a search url string with the selected text as query:
-                 System.Diagnostics.Process.Start("http://unity3d.com/support/documentation/ScriptReference/30_search.html?q="
-                    + System.Web.HttpUtility.UrlEncode(editor.SelectedText));
+                switch (target)
+                {
+                    case Unity.Documentation.Manual:
+                        return this.Open(Unity.REMOTE_SEARCH_MANUAL_URI + System.Web.HttpUtility.UrlEncode(query));
+                    case Unity.Documentation.Reference:
+                        return this.Open(Unity.REMOTE_SEARCH_REFERENCE_URI + System.Web.HttpUtility.UrlEncode(query));
+                    case Unity.Documentation.ScriptReference:
+                        return this.Open(Unity.REMOTE_SEARCH_SCRIPT_REFERENCE_URI + System.Web.HttpUtility.UrlEncode(query));
+                }
             }
+            return false;
         }
+
+        public bool Open(string address)
+        {
+            if ( this.openInBrowser )
+            {
+                System.Diagnostics.Process.Start(address);
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
     }
+
 }
